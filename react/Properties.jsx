3356caf647b79419ccdbf7dff9149547b950ba96
VaclavPilat <pilatvacl@seznam.cz>
2022-08-18 14:52:13 +0200

    Showing element size in properties

--- react/Properties.jsx
+++ react/Properties.jsx
@@ -18,12 +18,11 @@ class Properties extends React.Component {
         var styles = this.parseStyleProperties(this.props.currentElement.getAttribute("style"));
         return(
             <Wrapper>
-                {JSON.stringify(styles)}
+                <VectorProperty name={"Size"} x={styles.width != null ? styles.width : "0"} y={styles.height != null ? styles.height : "0"} locked={false} />
                 {/*
                 <VectorProperty name={"Position"} x={0} y={0} z={0} locked={false} />
                 <VectorProperty name={"Rotation"} x={0} y={0} z={0} locked={false} />
                 <VectorProperty name={"Scale"} x={1} y={1} z={1} locked={true} />
-                <VectorProperty name={"Size"} x={100} y={100} locked={false} />
                 <CustomProperty name={"background-color"} value={"red"} />
                 <CustomProperty name={"border"} value={"3px solid black"} />
                 <NewPropertyButtons />*/}
@@ -42,13 +41,13 @@ class VectorProperty extends React.Component {
             <div class="input-group mb-3">
                 <span class="input-group-text bg-secondary bg-opacity-75 text-white w-25 border-0">{this.props.name}</span>
                 <span class="input-group-text bg-secondary bg-opacity-75 text-white border-top-0 border-bottom-0 border-end-0">X</span>
-                <input type="number" class="form-control bg-dark text-white border-secondary" placeholder="0" defaultValue={this.props.x} />
+                <input type="text" class="form-control bg-dark text-white border-secondary" placeholder="0" value={this.props.x} />
                 <span class="input-group-text bg-secondary bg-opacity-75 text-white border-0">Y</span>
-                <input type="number" class="form-control bg-dark text-white border-secondary" placeholder="0" defaultValue={this.props.y} />
+                <input type="text" class="form-control bg-dark text-white border-secondary" placeholder="0" value={this.props.y} />
                 {this.props.z != null && (
                     <Wrapper>
                         <span class="input-group-text bg-secondary bg-opacity-75 text-white border-0">Z</span>
-                        <input type="number" class="form-control bg-dark text-white border-secondary" placeholder="0" defaultValue={this.props.z} />
+                        <input type="text" class="form-control bg-dark text-white border-secondary" placeholder="0" value={this.props.z} />
                     </Wrapper>
                 )}
                 <button class="btn btn-warning">
