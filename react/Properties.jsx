df5aee1915716447b38120b156c9bd6980b2147d
VaclavPilat <pilatvacl@seznam.cz>
2022-08-18 20:49:17 +0200

    Setting all transform values in Properties

--- react/Properties.jsx
+++ react/Properties.jsx
@@ -55,6 +55,35 @@ class Properties extends React.Component {
         }
         return transform;
     }
+    // Set new transform
+    setNewTransform = () => {
+        var transformValue = `translateX(` + this.transform.translate.x + `) translateY(` + this.transform.translate.y + `) translateZ(` + this.transform.translate.z
+            + `) rotateX(` + this.transform.rotate.x + `) rotateY(` + this.transform.rotate.y + `) rotateZ(` + this.transform.rotate.z
+            + `) scaleX(` + this.transform.scale.x + `) scaleY(` + this.transform.scale.y + `) scaleZ(` + this.transform.scale.z + `)`;
+        this.props.currentElement.style.transform = transformValue;
+        this.props.updateApplication();
+    }
+    // Setting new translation
+    setNewPosition = (x, y, z) => {
+        this.transform.translate.x = x;
+        this.transform.translate.y = y;
+        this.transform.translate.z = z;
+        this.setNewTransform();
+    }
+    // Setting new rotation
+    setNewRotation = (x, y, z) => {
+        this.transform.rotate.x = x;
+        this.transform.rotate.y = y;
+        this.transform.rotate.z = z;
+        this.setNewTransform();
+    }
+    // Setting new scale
+    setNewScale = (x, y, z) => {
+        this.transform.scale.x = x;
+        this.transform.scale.y = y;
+        this.transform.scale.z = z;
+        this.setNewTransform();
+    }
     // Setting new element size
     setNewSize = (width, height) => {
         this.props.currentElement.style.width = width;
@@ -67,9 +96,9 @@ class Properties extends React.Component {
         this.transform = this.getTransformObject(styles);
         return(
             <Wrapper>
-                <VectorProperty name={"Position"} x={this.transform.translate.x} y={this.transform.translate.y} z={this.transform.translate.z} locked={false} />
-                <VectorProperty name={"Rotation"} x={this.transform.rotate.x} y={this.transform.rotate.y} z={this.transform.rotate.z} locked={false} />
-                <VectorProperty name={"Scale"} x={this.transform.scale.x} y={this.transform.scale.y} z={this.transform.scale.z} locked={true} />
+                <VectorProperty name={"Position"} x={this.transform.translate.x} y={this.transform.translate.y} z={this.transform.translate.z} locked={false} onChangeHandler={this.setNewPosition} />
+                <VectorProperty name={"Rotation"} x={this.transform.rotate.x} y={this.transform.rotate.y} z={this.transform.rotate.z} locked={false} onChangeHandler={this.setNewRotation} />
+                <VectorProperty name={"Scale"} x={this.transform.scale.x} y={this.transform.scale.y} z={this.transform.scale.z} locked={true} onChangeHandler={this.setNewScale} />
                 <VectorProperty name={"Size"} x={styles.width ? styles.width : "0"} y={styles.height ? styles.height : "0"} locked={false} onChangeHandler={this.setNewSize} />
                 {Object.keys(styles).filter(key => key !== "width" && key != "height" && key != "transform").map((property, i) => (
                     <CustomProperty name={property} value={styles[property]} />
