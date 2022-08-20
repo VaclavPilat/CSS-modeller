f83ef076b864e0132dd9418e2df270d58122d10a
VaclavPilat <pilatvacl@seznam.cz>
2022-08-20 21:35:15 +0200

    Replaced button with div in TreeviewItem to allow dragging

--- react/Treeview.jsx
+++ react/Treeview.jsx
@@ -25,11 +25,11 @@ class TreeviewItem extends React.Component {
         var ID = createUniqueID();
         return (
             <li class="m-0 p-0">
-                <div class="input-group m-0 mt-1 p-0">
+                <div class="input-group m-0 mt-1 p-0" draggable="true">
                     {this.props.element.children.length > 0 && (
                         <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target={"#" + ID}><i class="bi bi-chevron-down"></i></button>
                     )}
-                    <button class={"btn input-group-text flex-grow-1 text-uppercase text-start " + (this.props.current ? "bg-white bg-opacity-50 border-white border-opacity-50 text-black" : "bg-secondary bg-opacity-75 border-secondary border-opacity-75 text-white")} onClick={this.setCurrentElement}>{this.props.element.getAttribute("data-modeller-title").replace("model-", "")}</button>
+                    <div class={"btn input-group-text flex-grow-1 text-uppercase text-start " + (this.props.current ? "bg-white bg-opacity-50 border-white border-opacity-50 text-black" : "bg-secondary bg-opacity-75 border-secondary border-opacity-75 text-white")} onClick={this.setCurrentElement}>{this.props.element.getAttribute("data-modeller-title").replace("model-", "")}</div>
                     {!this.props.root ? (
                         <button class="btn btn-danger" onClick={this.removeElement}><i class="bi bi-trash-fill"></i></button>
                     ) : ( null )}
